9045d2c1bfbb1736ed121352bde1617d6c96fe0e
Harsh Dalwadi <Harsh Dalwadi>
2016-09-02 00:42:49 +0530

    Version 1.0

--- js/global.js
+++ js/global.js
@@ -7,7 +7,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 $( function() {
 // Add background image
 	$.backstretch('images/road2.jpg');
-	var endDate = "December  27, 2017 15:03:25";
+	var endDate = "September  12, 2016 00:00:00";
 	$('.countdown.simple').countdown({ date: endDate });
 	$('.countdown.styled').countdown({
 	  date: endDate,
